9bdf9aebb52e07d5a1ec2fe1657b1f5aa05f2b4c
jazzrider <root@nat-tvwna-inside-visitornet101-c-23842.princeton.org>
2016-04-02 02:25:22 -0400

    added more states for osc

--- mainCode.py
+++ mainCode.py
@@ -12,7 +12,8 @@ import serial
 glist = glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*') + glob.glob('/dev/tty.u*')
 ser = serial.Serial(glist[0], 9600)
 #case statement
-command = ['AUTORange','?'] #get from lambda function
+command = ['AUTORange','?'] 
+isquery = 0
 #get copy of screen as BMP image, command = ['copy']
 def copy():
 	ser.write('HARDCopy STARt\r')
@@ -31,18 +32,61 @@ def CH():
 	s = 'CH' + command[1] + ':' + command[2] + command[3] + '\r'
 	ser.write(s)
 #cursor settings, command = ['cursor',<option>,<value>]
-#'?'-all settings, 'FUNCtion HBArs/OFF/VBArs' - can add ':DELTA/:POSITION/:UNIts?', 
+#'?'-all settings, 'FUNCtion HBArs/OFF/VBArs' - can add ':DELTA/:POSITION/:UNIts?'
+# <value> should start with a space unless '?'
+def cursor():
+	s = 'CURSor:' + command[1] + command[2] + '\r'
+	ser.write(s)
+#get waveform data, command = ['curve',{' <Block>',' <asc curve','?'}]
+def curve():
+	s = 'CURVe' + command[1] + '\r'
+	ser.write(s)
+#display settings, command = ['display',<option>,<value>]
+#'?', 'BRIGHTness <num>', CONTRast <num>, FORMat XY/YT, INVert ON/OFF, STYle DOTs/VECtors
+#<value< should start with a space unless '?'
+def display():
+	s = 'DISplay:' + command[1] + command[2] + '\r'
+	ser.write(s)
+#harmonics settings and analysis, command = ['harmonics',<option>,<value>]
+#'?', ENABle ON/OFF, SELect <num>, SHOW ALL/ODD/EVEN, SOUrce CH<x>
+#query only: FREquency, HRMS, PERCent, PHAse, RMS, THDF
+#<value> should start with a space unless '?'
+def harmonics():
+	s = 'HARmonics:' + command[1] + command[2] + '\r'
+	ser.write(s)
+#horizontal settings (assumes main), command = ['horizontal',<option>,<value>]
+#'?', POSition <num>, SCAle <num>, VIEW MAIn/WINDOW/ZONE
+#<value> should start with a space unless '?'
+def horizontal():
+	s = 'HORizontal:' + command[1] + command[2] + '\r'
+	ser.write(s)
+#measurements, command = ['measurement',<meas numb>,<option>,<value>]
+#<meas numb> specifies the measurement 1 through 5
+#'?',SOUrce CH<y>, TYPE FREQuency/MEAN/PERIod/PK2pk/CRMs/MINImum/MAXImum/RISe/FALL/PWIdth/NWIdth/NONe
+#query only: UNIts, VALue
+#<value> should start with a space unless '?'
+def measurements():
+	s = 'MEASUrement:MEAS' + command[1] +':' + command[2] + '\r'
+	ser.write(s)
 
 switcher = {
 	'copy': copy,
 	'AUTORange': AUTORange,
 	'AUTOSet': AUTOSet,
 	'CH': CH,
-	'cursor': cursor
+	'cursor': cursor,
+	'curve': curve,
+	'display': display,
+	'harmonics': harmonics,
+	'horizontal': horizontal,
+	'measurement': measurement
 }
 func = switcher.get(command[0], lambda: "not valid")
 func()
 
-#result = ser.readline()
+if isquery:
+	result = ser.readline()
+	isquery = 0
+
 #change scale
 #ser.write('CH1:SCAle 10e-2\r')
