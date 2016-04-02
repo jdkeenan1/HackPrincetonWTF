686f141647caf6f23fdd870b2ff76daed9070f0e
root <root@raspberrypi.(none)>
2016-04-02 06:24:21 +0000

    finished

--- functionGenerator.py
+++ functionGenerator.py
@@ -13,30 +13,28 @@ import visa
 rm = visa.ResourceManager('@py')
 inst = rm.open_resource('USB0::0x0957::0x0407::SG44000971::INSTR')
 #query
-
-
-
-
-
 #case statement
 #command = ['AUTORange','?'] #get from lambda function
 #example of applying a command
+outputString = []
+
 def applyFunction():
-	def SINusoid:
-		outputString += ":SINusoid"
-	def SQUare:
-		outputString += ":SQUare"
-	def RAMP:
-		outputString += ":RAMP"
-	def PULSe:
-		outputString += ":PULSe"
-	def NOISe:
-		outputString += ":NOISe"
-	def DC:
-		outputString += ":DC"
-	def user:
-		outputString += ":user"
-	outputString = "APPLy"
+	outputString = ['APPLy']
+	
+	def SINusoid():
+		outputString.append(':SINusoid')
+	def SQUare():
+		outputString.append(':SQUare')
+	def RAMP():
+		outputString.append(':RAMP')
+	def PULSe():
+		outputString.append(':PULSe')
+	def NOISe():
+		outputString.append(':NOISe')
+	def DC():
+		outputString.append(':DC')
+	def USER():
+		outputString.append(':USER')
 	switcher1 = {
 		'sin': SINusoid,
 		'square': SQUare,
@@ -50,17 +48,19 @@ def applyFunction():
 	func1()
 	for i, item in enumerate(command):
 		if i > 1:
-			outputString += " "+item
-	inst.write(outputString)
+			outputString.append(" "+item+",")
+	output = ''.join(outputString)
+	print output
+	inst.write(output)
 while 1:
 	var = raw_input("Please enter something: ")
 	command = var.split()
 	switcher = {
-		'apply': applyFunction,
-		'AUTORange': AUTORange,
-		'AUTOSet': AUTOSet,
-		'CH': CH,
-		'cursor': cursor
+		'apply': applyFunction
+		#'AUTORange': AUTORange,
+		#'AUTOSet': AUTOSet,
+		#'CH': CH,
+		#'cursor': cursor
 	}
 	func = switcher.get(command[0], lambda: "not valid")
 	func()
